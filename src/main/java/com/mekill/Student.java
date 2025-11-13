fef37c7c4ba6bb305f5eacc577bab5fcc47420cf
mekill404 <hei.mahery.55@gmail.com>
2025-11-13 14:12:45 +0300

    Refactor Student class to use Lombok annotations for getters and constructors

--- src/main/java/com/mekill/Student.java
+++ src/main/java/com/mekill/Student.java
@@ -1,5 +1,14 @@
 package com.mekill;
 
-public class Student {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@AllArgsConstructor
+public class Student 
+{
+  private int id_student;
+  private String name, first_name, date_naissance, email, groupe;
+  private Tutor tutor;
 }
