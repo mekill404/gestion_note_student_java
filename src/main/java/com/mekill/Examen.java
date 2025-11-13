183ed33e2dbfaf7b0a7634df8ea5f9d911fceac3
mekill404 <hei.mahery.55@gmail.com>
2025-11-13 14:36:00 +0300

    Add Examen class with fields for exam details

---
+++ src/main/java/com/mekill/Examen.java
@@ -0,0 +1,13 @@
+package com.mekill;
+
+import java.sql.Date;
+import java.sql.Time;
+
+public class Examen 
+{
+  private int id_exam, coeficient;
+  private String title;
+  private Courses course;
+  private Date date_exam;
+  private Time time_exam;
+}
