6cbd840b8178f8634253294bdc1d2f91b00c9ea5
mekill404 <hei.mahery.55@gmail.com>
2025-11-13 14:55:07 +0300

    Refactor Tutor class to use Lombok annotations and correct field types

--- src/main/java/com/mekill/Tutor.java
+++ src/main/java/com/mekill/Tutor.java
@@ -1,7 +1,16 @@
 package com.mekill;
 
-public class Tutor 
-{
-  private int id_tutor;
-  private String name, fristname, birthday, email, tel_number, description_link; 
+import java.time.LocalDate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tutor {
+  private Integer id_tutor;
+  private LocalDate birthday;
+  private String name, firstname, email, tel_number, description_link;
 }
