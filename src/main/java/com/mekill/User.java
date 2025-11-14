6079635cf02b96dd472d498ab80052755b70a760
mekill404 <hei.mahery.55@gmail.com>
2025-11-14 09:21:58 +0300

    refactor(User): apply Lombok annotations for cleaner code

--- src/main/java/com/mekill/User.java
+++ src/main/java/com/mekill/User.java
@@ -2,12 +2,16 @@ package com.mekill;
 
 import java.time.LocalDate;
 
-public class User
-{
-    private int id;
-    private String  name;
-    private String  first_name;
-    private LocalDate birthdate;
-    private String phone_number;
-    private String email;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class User {
+  private int id;
+  private String name;
+  private String first_name;
+  private LocalDate birthdate;
+  private String phone_number;
+  private String email;
 }
