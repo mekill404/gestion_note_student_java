d135e0f9cec4542a235f7baf42c506dd7a912f8d
mekill404 <hei.mahery.55@gmail.com>
2025-11-14 08:26:22 +0300

    refactor(Courses): add lombok to display constructor

--- src/main/java/com/mekill/Courses.java
+++ src/main/java/com/mekill/Courses.java
@@ -1,8 +1,12 @@
 package com.mekill;
 
 import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Courses 
 {
   private int id_courses;
