c613b3cc5f1d9740064a4b13bbc47e741c9ef85c
mekill404 <hei.mahery.55@gmail.com>
2025-11-14 09:25:06 +0300

    refactor(User): add @NoArgsConstructor for improved object creation

--- src/main/java/com/mekill/User.java
+++ src/main/java/com/mekill/User.java
@@ -4,9 +4,11 @@ import java.time.LocalDate;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
   private int id;
   private String name;
