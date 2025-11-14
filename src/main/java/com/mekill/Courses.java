c175fa23be36dfa4a9f17031f8113d1815cc69eb
mekill404 <hei.mahery.55@gmail.com>
2025-11-14 08:27:12 +0300

    refactor(Courses): remove NoArgsConstructor

--- src/main/java/com/mekill/Courses.java
+++ src/main/java/com/mekill/Courses.java
@@ -2,11 +2,9 @@ package com.mekill;
 
 import lombok.Data;
 import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-@NoArgsConstructor
 public class Courses 
 {
   private int id_courses;
